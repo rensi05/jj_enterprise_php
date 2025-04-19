73b58dce10be3a86311805dd91d3034a9be964f4
Rensi <rensi.baldha1905@gmail.com>
2025-04-19 16:20:21 +0530

    Text changes

--- resources/views/user/item/item.blade.php
+++ resources/views/user/item/item.blade.php
@@ -70,7 +70,7 @@
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Import Customers</h5>
+                <h5 class="modal-title">Import Items</h5>
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
             <div class="modal-body">
